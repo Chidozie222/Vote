f0ae1a6200d7208b115a52991ac7d60c232d50ad
Chidozie222 <ibennejohn3@gmail.com>
2023-10-27 04:39:21 -0700

    working

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors')
+const mongoose = require('mongoose')
 const home = require('./home')
 const app = express();
 app.use(express.json())
 app.use(cors())
 app.use('*', home)
-
+require('dotenv').config()
 app.listen(2000, (err, res) => {
     console.log('this port is running on 2000');
 })
